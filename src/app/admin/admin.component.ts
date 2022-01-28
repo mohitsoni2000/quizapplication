4e8eb5f5ed2792097386bec75dfdbc40f1e183e5
mohitsoni2000 <pro.mohit2000@gmail.com>
2022-01-28 21:01:17 +0530

    for radio and localstorage

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,44 +3,54 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
-addQuestions: any = {
-  "questions": {
-    "questionText": "",
-    "options": [
-      {
-        "text": "",
-        "correct": "true"
-      },
-      {
-        "text": "",
-        "correct": "true"
+  addQuestions: any = {
+    questions: {
+      questionText: '',
+      options: [
+        {
+          text: '',
+          correct: true,
+        },
+        {
+          text: '',
+          correct: false,
+        },
+        {
+          text: '',
+          correct: false,
+        },
+        {
+          text: '',
+          correct: false,
+        },
+      ],
+    },
+  };
+  constructor() {}
 
-      },
-      {
-        "text": "",
-        "correct": "true"
-
-      },
-      {
-        "text": "",
-        "correct": "true"
-
-      }
-    ]
+  ngOnInit(): void {}
+  addedQuestion() {
+    console.log(this.addQuestions);
+    // console.log(this.addQuestions.questions.options);
+    console.log(this.addQuestions.questions.answer);
+   this.addQuestions = localStorage.setItem("Questions" , JSON.stringify( this.addQuestions.questions));
+ this.addQuestions = [this.addQuestions, ...this.addQuestions ]
+// this.added = this.addQuestions.questions;
   }
+added(){
+
 }
-  constructor() { }
 
-  ngOnInit(): void {
+
+  onRadioChange(i: any) {
+    console.log(console.log(this.addQuestions.questions.options[i].correct));
+
+    this.addQuestions.questions.options.forEach((el: any) => {
+      el.correct = false;
+    });
+    this.addQuestions.questions.options[i].correct = true;
   }
-addedQuestion(){
-  }
-
-radioSelected(){
-
-}
-
 }
